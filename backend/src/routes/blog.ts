d0045860d7f895fad7b1192f7c2bc116191fe200
Abhinav Sharma <abhinavsharma392@gmail.com>
2024-03-24 23:30:58 +0530

    Adding backend, frontend, and common module.

---
+++ backend/src/routes/blog.ts
@@ -0,0 +1,137 @@
+import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { verify } from "hono/jwt";
+import { createBlogInput, updateBlogInput } from "@abhinav08/medium-blogging";
+
+export const blogRouter = new Hono<{
+	Bindings:{
+		DATABASE_URL: string;
+		JWT_SECRET: string;
+	},
+    Variables:{
+        userId: string;
+    }
+}>();
+
+blogRouter.use("/*", async(c, next) => {
+    const authHeader = c.req.header('Authorization');
+	if (!authHeader) {
+		c.status(401);
+		return c.json({ error: "unauthorized" });
+	}
+	const user = await verify(authHeader, c.env.JWT_SECRET);
+	if (!user) {
+		c.status(401);
+		return c.json({ error: "unauthorized" });
+	}
+	c.set('userId', user.id);
+	await next();
+})
+
+blogRouter.post('/', async(c) => {
+	const body = await c.req.json();
+    const { success } = createBlogInput.safeParse(body);
+    if(!success){
+        c.status(411);
+        return c.json({
+            message:"Inputs not correct"
+        })
+    }
+    const authorId = c.get("userId");
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env.DATABASE_URL,
+	}).$extends(withAccelerate())
+
+    const blog = await prisma.blog.create({
+        data:{
+            title: body.title,
+            content: body.content,
+            authorId: Number(authorId)
+        }
+    })
+	return c.json({
+        id: blog.id
+    })
+})
+
+blogRouter.put('/', async(c) => {
+    const body = await c.req.json();
+    const { success } = updateBlogInput.safeParse(body);
+    if(!success){
+        c.status(411);
+        return c.json({
+            message:"Inputs not correct"
+        })
+    }
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env.DATABASE_URL,
+	}).$extends(withAccelerate())
+
+    const blog = await prisma.blog.update({
+        where:{
+            id: body.id
+        },
+        data:{
+            title: body.title,
+            content: body.content
+        }
+    })
+	return c.json({
+        id: blog.id
+    })
+})
+
+blogRouter.get('/bulk', async(c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env.DATABASE_URL,
+	}).$extends(withAccelerate())
+    const blogs = await prisma.blog.findMany({
+        select:{
+            content:true,
+            title:true,
+            id:true,
+            author:{
+                select:{
+                    name:true
+                }
+            }
+        }
+    });
+
+    return c.json({
+        blogs
+    })
+})
+
+blogRouter.get('/:id', async(c) => {
+	const id = c.req.param("id")
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env.DATABASE_URL,
+	}).$extends(withAccelerate())
+
+    try{
+        const blog = await prisma.blog.findFirst({
+        where:{
+            id:Number(id)
+        },select:{
+            id:true,
+            title:true,
+            content:true,
+            author:{
+                select:{
+                    name:true 
+                }
+            }
+        }
+    })
+	return c.json({
+        blog
+    })
+    }catch(e){
+        c.status(411);
+        c.json({
+            messaage:"Error while fetching blog post"
+        });
+    }   
+})
---
+++ frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL ="https://medium-blogging.abhinavsharma392.workers.dev";
